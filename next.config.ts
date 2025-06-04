6caa33f3285f9f4d8d2fe29caafaf1fe4170524e
aIcoder504 <139570466+aIcoder504@users.noreply.github.com>
2025-06-04 12:05:25 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    optimizeCss: false, // This fixes the critters error
+    optimizeCss: false,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push({
+        'better-sqlite3': 'commonjs better-sqlite3',
+      });
+    }
+    return config;
   },
 };
 
